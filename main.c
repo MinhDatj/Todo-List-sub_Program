acfbe5afc9ee113ec718d4fc488fdab3bf61e0f2
MinhDatj <nlmdat1204@gmail.com>
2025-03-21 23:20:33 +0700

    [ADD] Create adding_task + response function with some definitions

--- main.c
+++ main.c
@@ -5,6 +5,9 @@
 #define MAX_TITLE 50
 #define MAX_TASK 10
 #define LAST_OPTION 4
+#define ADDING_SUCCEEDED 1
+#define FIXING_SUCCEEDED 2
+#define DELETING_SUCCEEDED 3
 
 int INPUT_get_option(void) {
 	int is_valid = 0;
@@ -17,13 +20,44 @@ int INPUT_get_option(void) {
 	}
 }
 
+int INTPUT_get_progress(void) {
+	int is_valid = 0;
+	int progress;
+	while (!is_valid) {
+		printf("Your progress [0-100]: ");
+		scanf("%d", &progress);
+		is_valid = (progress >= 0 && progress <= 100);
+		if (is_valid) return progress;
+	}
+}
+
+int INPUT_new_task(int id[], char list[][MAX_TITLE], int progress[], int *list_length) {
+	if (*list_length == MAX_TASK) return 0;
+
+	printf("\t\nYour task: ");
+	scanf("%49s%*c", list[*list_length]);
+	progress[*list_length] = INTPUT_get_progress();
+	(*list_length)++;
+	return ADDING_SUCCEEDED;
+}
+
+void OUTPUT_response(int signal) {
+	if (!signal) printf("\nFailed!");
+
+	switch (signal)
+	{
+	case 1:
+		printf("\nAdding task successfully!");
+		break;
+	}
+}
 
 int main() {
 	int id[MAX_TASK];
 	char list[MAX_TASK][MAX_TITLE];
 	int progress[MAX_TASK];
 	int list_length = 0;
-	int option, ID;
+	int option, ID, signal;
 
 	printf("\nTO DO LIST");
     printf("\nPlease select an option:");
@@ -40,6 +74,8 @@ int main() {
 
 	switch (option) {
 		case 1:
+			signal = INPUT_new_task(id, list, progress, &list_length);
+			OUTPUT_response(signal);
 			break;
 		case 2:
 			break;

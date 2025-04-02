55191a7926dfb50316d5426c65f56263e1c78e3a
MinhDatj <nlmdat1204@gmail.com>
2025-04-02 09:10:46 +0700

    [UPDATE] executable file's name

--- utils/build.sh
+++ utils/build.sh
@@ -3,4 +3,4 @@
 if [ ! -d "build" ]; then
     mkdir build
 fi
-gcc main.c -o build/datj_todolist_app -std=c99 -Wpedantic -Wall -Wextra -Waggregate-return -Wbad-function-cast -Wcast-align -Wcast-qual -Wdeclaration-after-statement -Wfloat-equal -Wformat=2 -Wlogical-op -Wmissing-declarations -Wmissing-declarations -Wmissing-prototypes -Wmissing-include-dirs -Wnested-externs -Wpointer-arith -Wredundant-decls -Wsequence-point -Wshadow -Wstrict-prototypes -Wswitch -Wundef -Wunreachable-code -Wunused-but-set-parameter -Wwrite-strings
+gcc main.c -o build/datj_todolist_app-v0.0.1-ubuntu22.04-x64-gcc11.4.0 -std=c99 -Wpedantic -Wall -Wextra -Waggregate-return -Wbad-function-cast -Wcast-align -Wcast-qual -Wdeclaration-after-statement -Wfloat-equal -Wformat=2 -Wlogical-op -Wmissing-declarations -Wmissing-declarations -Wmissing-prototypes -Wmissing-include-dirs -Wnested-externs -Wpointer-arith -Wredundant-decls -Wsequence-point -Wshadow -Wstrict-prototypes -Wswitch -Wundef -Wunreachable-code -Wunused-but-set-parameter -Wwrite-strings
--- utils/run.sh
+++ utils/run.sh
@@ -1,2 +1,2 @@
 #!/bin/bash
-./build/datj_todolist_app
+./build/datj_todolist_app-v0.0.1-ubuntu22.04-x64-gcc11.4.0
